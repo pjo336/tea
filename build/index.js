8e64d9c6934b171d545d3505041a46f45796630b
Peter Johnston <pemjohns@gmail.com>
2019-05-27 19:23:08 -0600

    .gitignore fix for build folder

--- build/index.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin / env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const cli_1 = require("./cli");
-cli_1.default.parse(process.argv);
-//# sourceMappingURL=index.js.map
